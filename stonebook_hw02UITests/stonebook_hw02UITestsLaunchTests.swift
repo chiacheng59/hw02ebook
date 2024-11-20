5506911fb749ed8e56b515dd4289d038185110d5
peter li <peter870509@gmail.com>
2024-11-20 13:43:27 +0800

    Initial Commit

---
+++ stonebook_hw02UITests/stonebook_hw02UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  stonebook_hw02UITestsLaunchTests.swift
+//  stonebook_hw02UITests
+//
+//  Created by 李家誠 on 2024/11/20.
+//
+
+import XCTest
+
+final class stonebook_hw02UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
